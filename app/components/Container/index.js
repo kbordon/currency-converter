2112c48e6a59d24746a495e3a3fdcbb2e38809e1
Kimberly Bordon <kbordon@gmail.com>
2018-04-12 13:04:39 -0700

    Style home screen container

--- app/components/Container/index.js
+++ app/components/Container/index.js
@@ -1,2 +1,4 @@
 import Container from './Container';
 import styles from './styles';
+
+export { Container, styles };
--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Home from './screens/Home';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
+// need before app builds
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
 })
